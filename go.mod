bcab598df6b813c236568a19872b41a6c40bd481
skar404 <skar404@gmail.com>
2020-06-07 18:10:41 +0300

    init web server

--- go.mod
+++ go.mod
@@ -1,8 +1,13 @@
-module custom-podcasts
+module github.com/skar404/self-casts
 
 go 1.14
 
 require (
-	github.com/nareix/joy4 v0.0.0-20181022032202-3ddbc8f9d431 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/labstack/echo/v4 v4.1.16
 	github.com/rylio/ytdl v0.6.2
+	golang.org/x/net v0.0.0-20200602114024-627f9648deb9 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "custom-podcasts/src/download"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type DownloadView struct {
+	UUID string `json:"uuid" form:"uuid" query:"uuid"`
+}
+type GetAudioView struct {
+	Link string `json:"link" form:"link" query:"link"`
+}
+
+func Download(c echo.Context) error {
+	d := DownloadView{
+		UUID: "uuid",
+	}
+	return c.JSON(http.StatusOK, d)
+}
+
+func GetAudio(c echo.Context) error {
+	d := GetAudioView{
+		Link: "http://test/uuid.mp3",
+	}
+	return c.JSON(http.StatusOK, d)
+}
 
 func main() {
-	download.GetVideo("https://www.youtube.com/watch?v=DxdJ_-T9ZVw")
+	e := echo.New()
+
+	v1 := e.Group("/api/v1/")
+	{
+		v1.POST("download", Download)
+		v1.GET("audio/:uuid", GetAudio)
+	}
+
+	e.Logger.Fatal(e.Start("127.0.0.1:1323"))
+	//download.GetVideo("https://www.youtube.com/watch?v=DxdJ_-T9ZVw")
 }
